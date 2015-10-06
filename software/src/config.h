af2084e160e9ef4558a1a681b9f86c7d7e6b481d
Olaf Lüke <olaf@tinkerforge.com>
2015-10-06 14:27:52 +0200

    Add initial implementation for veml6070

---
+++ software/src/config.h
@@ -0,0 +1,79 @@
+/* uv-light-bricklet
+ * Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * config.h: UV Light Bricklet specific configuration
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "bricklib/drivers/board/sam3s/SAM3S.h"
+
+#include "uv-light.h"
+
+#define BRICKLET_FIRMWARE_VERSION_MAJOR 2
+#define BRICKLET_FIRMWARE_VERSION_MINOR 0
+#define BRICKLET_FIRMWARE_VERSION_REVISION 0
+
+#define BRICKLET_HARDWARE_VERSION_MAJOR 1
+#define BRICKLET_HARDWARE_VERSION_MINOR 0
+#define BRICKLET_HARDWARE_VERSION_REVISION 0
+
+#define BRICKLET_DEVICE_IDENTIFIER 265
+
+#define BOARD_MCK 64000000
+
+#define PIN_ACK (BS->pin1_ad)
+#define PIN_SDA (BS->pin2_da)
+#define PIN_SCL (BS->pin3_pwm)
+
+#define LOGGING_LEVEL LOGGING_DEBUG
+#define DEBUG_BRICKLET 0
+
+#define BRICKLET_VALUE_APPLIED_OUTSIDE
+#define BRICKLET_HAS_SIMPLE_SENSOR
+#define BRICKLET_NO_OFFSET
+#define INVOCATION_IN_BRICKLET_CODE
+#define NUM_SIMPLE_VALUES 1
+
+#define SIMPLE_VALUE_TYPE uint16_t
+
+typedef struct {
+	uint32_t value[NUM_SIMPLE_VALUES];
+	uint32_t last_value[NUM_SIMPLE_VALUES];
+
+	uint32_t signal_period[NUM_SIMPLE_VALUES];
+	uint32_t signal_period_counter[NUM_SIMPLE_VALUES];
+
+	uint32_t threshold_debounce;
+	uint32_t threshold_period_current[NUM_SIMPLE_VALUES];
+	uint32_t  threshold_min[NUM_SIMPLE_VALUES];
+	uint32_t  threshold_max[NUM_SIMPLE_VALUES];
+	char     threshold_option[NUM_SIMPLE_VALUES];
+
+	uint32_t  threshold_min_save[NUM_SIMPLE_VALUES];
+	uint32_t  threshold_max_save[NUM_SIMPLE_VALUES];
+	char     threshold_option_save[NUM_SIMPLE_VALUES];
+
+	uint32_t tick;
+} BrickContext;
+
+#endif
---
+++ software/src/uv-light.c
@@ -0,0 +1,230 @@
+/* uv-light-bricklet
+ * Copyright (C) 2015 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * uv-light.c: Implementation of UV Light Bricklet messages
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "uv-light.h"
+
+#include "bricklib/bricklet/bricklet_communication.h"
+#include "bricklib/utility/util_definitions.h"
+#include "bricklib/drivers/adc/adc.h"
+#include "brickletlib/bricklet_entry.h"
+#include "brickletlib/bricklet_simple.h"
+#include "config.h"
+
+#define I2C_HALF_CLOCK_400KHZ 1250  // 2.5us per clock
+
+#define ILLUMINANCE_AVERAGE 50
+
+#define SIMPLE_UNIT_UV_INDEX 0
+#define SIMPLE_UNIT_IR_VALUE 1
+#define SIMPLE_UNIT_ILLUMINANCE 2
+
+const SimpleMessageProperty smp[] = {
+	{SIMPLE_UNIT_UV_INDEX, SIMPLE_TRANSFER_VALUE, SIMPLE_DIRECTION_GET}, // TYPE_GET_UV_INDEX
+	{SIMPLE_UNIT_UV_INDEX, SIMPLE_TRANSFER_PERIOD, SIMPLE_DIRECTION_SET}, // TYPE_SET_UV_INDEX_CALLBACK_PERIOD
+	{SIMPLE_UNIT_UV_INDEX, SIMPLE_TRANSFER_PERIOD, SIMPLE_DIRECTION_GET}, // TYPE_GET_UV_INDEX_CALLBACK_PERIOD
+	{SIMPLE_UNIT_UV_INDEX, SIMPLE_TRANSFER_THRESHOLD, SIMPLE_DIRECTION_SET}, // TYPE_SET_UV_INDEX_CALLBACK_THRESHOLD
+	{SIMPLE_UNIT_UV_INDEX, SIMPLE_TRANSFER_THRESHOLD, SIMPLE_DIRECTION_GET}, // TYPE_GET_UV_INDEX_CALLBACK_THRESHOLD
+	{0, SIMPLE_TRANSFER_DEBOUNCE, SIMPLE_DIRECTION_SET}, // TYPE_SET_DEBOUNCE_PERIOD
+	{0, SIMPLE_TRANSFER_DEBOUNCE, SIMPLE_DIRECTION_GET}, // TYPE_GET_DEBOUNCE_PERIOD
+};
+
+const SimpleUnitProperty sup[] = {
+	{NULL, SIMPLE_SIGNEDNESS_UINT, FID_UV_INDEX, FID_UV_INDEX_REACHED, SIMPLE_UNIT_UV_INDEX}, // uv index
+};
+
+const uint8_t smp_length = sizeof(smp);
+
+
+void invocation(const ComType com, const uint8_t *data) {
+	simple_invocation(com, data);
+
+	if(((MessageHeader*)data)->fid > FID_LAST) {
+		BA->com_return_error(data, sizeof(MessageHeader), MESSAGE_ERROR_CODE_NOT_SUPPORTED, com);
+	}
+}
+
+void constructor(void) {
+	_Static_assert(sizeof(BrickContext) <= BRICKLET_CONTEXT_MAX_SIZE, "BrickContext too big");
+
+	PIN_SDA.type = PIO_INPUT;
+	PIN_SDA.attribute = PIO_PULLUP;
+	BA->PIO_Configure(&PIN_SDA, 1);
+
+	PIN_SCL.type = PIO_INPUT;
+	PIN_SCL.attribute = PIO_PULLUP;
+	BA->PIO_Configure(&PIN_SCL, 1);
+
+	PIN_ACK.type = PIO_INPUT;
+	PIN_ACK.attribute = PIO_PULLUP;
+	BA->PIO_Configure(&PIN_ACK, 1);
+
+	simple_constructor();
+
+	veml6070_write_configuration(VEML6070_CONF_ACK_DISABLE | VEML6070_CONF_INTEGRATION_ONE | VEML6070_CONF_STUDOWN_DISABLE);
+
+}
+
+void destructor(void) {
+	simple_destructor();
+}
+
+void tick(const uint8_t tick_type) {
+	if(tick_type & TICK_TASK_TYPE_CALCULATION) {
+		if(BC->tick % 100 == 0) {
+			BC->last_value[0] = BC->value[0];
+			BC->value[0] = veml6070_read_uv_index();
+		}
+	}
+
+	simple_tick(tick_type);
+}
+
+uint16_t veml6070_read_uv_index(void) {
+	uint16_t uv_value = 0;
+
+	i2c_start();
+	i2c_send_byte((I2C_ADDRESS1_VEML6070 << 1) | I2C_READ);
+	uv_value |= i2c_recv_byte(false);
+	i2c_stop();
+
+	i2c_start();
+	i2c_send_byte((I2C_ADDRESS2_VEML6070 << 1) | I2C_READ);
+	uv_value |= (i2c_recv_byte(false) << 8);
+	i2c_stop();
+
+	// 1 step = 5 µW/cm² = 5*1000/10000 mW/m² = 0.5 mW/m²
+	// UV Index = mW/25m² = uv_value * 5*1000 / (10000*25) = uv_value / 50
+	// => UV Index/100 = uv_value*2
+
+	// Max value for uv_value is 328*50 = 16400. So *2 is OK here.
+	return uv_value*2;
+}
+
+void veml6070_write_configuration(const uint8_t configuration) {
+	i2c_start();
+	i2c_send_byte((I2C_ADDRESS1_VEML6070 << 1) | I2C_WRITE);
+	i2c_send_byte(configuration);
+	i2c_stop();
+}
+
+bool i2c_scl_value(void) {
+	return PIN_SCL.pio->PIO_PDSR & PIN_SCL.mask;
+}
+
+void i2c_scl_high(void) {
+	PIN_SCL.pio->PIO_ODR = PIN_SCL.mask;
+	while(!i2c_scl_value()); // allow slave to clock-stretch
+}
+
+void i2c_scl_low(void) {
+	PIN_SCL.pio->PIO_OER = PIN_SCL.mask;
+}
+
+bool i2c_sda_value(void) {
+	return PIN_SDA.pio->PIO_PDSR & PIN_SDA.mask;
+}
+
+void i2c_sda_high(void) {
+	PIN_SDA.pio->PIO_ODR = PIN_SDA.mask;
+}
+
+void i2c_sda_low(void) {
+	PIN_SDA.pio->PIO_OER = PIN_SDA.mask;
+}
+
+void i2c_sleep_halfclock(void) {
+	SLEEP_NS(I2C_HALF_CLOCK_400KHZ);
+}
+
+void i2c_stop(void) {
+	i2c_scl_low();
+	i2c_sda_low();
+	i2c_sleep_halfclock();
+	i2c_scl_high();
+	i2c_sleep_halfclock();
+	i2c_sda_high();
+	i2c_sleep_halfclock();
+}
+
+void i2c_start(void) {
+	i2c_scl_high();
+	i2c_sleep_halfclock();
+	i2c_sda_low();
+	i2c_sleep_halfclock();
+}
+
+uint8_t i2c_recv_byte(bool ack) {
+	uint8_t value = 0;
+
+	for(int8_t i = 7; i >= 0; i--) {
+		i2c_scl_low();
+		i2c_sda_high(); // allow slave to read
+		i2c_sleep_halfclock();
+		i2c_scl_high();
+		if(i2c_sda_value()) {
+			value |= (1 << i);
+		}
+		i2c_sleep_halfclock();
+	}
+
+	// ACK
+	i2c_scl_low();
+	if(ack) {
+		i2c_sda_low();
+	} else {
+		i2c_sda_high();
+	}
+	i2c_sleep_halfclock();
+	i2c_scl_high();
+	i2c_sleep_halfclock();
+
+	return value;
+}
+
+bool i2c_send_byte(const uint8_t value) {
+	for(int8_t i = 7; i >= 0; i--) {
+		i2c_scl_low();
+		if((value >> i) & 1) {
+			i2c_sda_high();
+		} else {
+			i2c_sda_low();
+		}
+		i2c_sleep_halfclock();
+		i2c_scl_high();
+		i2c_sleep_halfclock();
+	}
+
+	i2c_sda_high(); // Make sure SDA is always released
+
+	// Wait for ACK
+	bool ret = false;
+
+	i2c_scl_low();
+	i2c_sleep_halfclock();
+	i2c_scl_high();
+	if(!i2c_sda_value()) {
+		ret = true;
+	}
+
+	i2c_sleep_halfclock();
+
+	return ret;
+}
