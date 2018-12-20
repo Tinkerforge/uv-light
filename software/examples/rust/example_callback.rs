f56c22c6d5d878373e358802dbd9c2723af9993a
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 17:01:01 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  uv_light_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, uv_light_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,23 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let uvl = UvLightBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let uv_light_receiver = uvl.get_uv_light_callback_receiver();
+    let uv_light_receiver = uvl.get_uv_light_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `uvl` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for uv_light in uv_light_receiver {           
-                		println!("UV Light: {} mW/m²", uv_light as f32 /10.0);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `uvl` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for uv_light in uv_light_receiver {
+            println!("UV Light: {} mW/m²", uv_light as f32 / 10.0);
+        }
+    });
 
-		// Set period for UV light receiver to 1s (1000ms).
-		// Note: The UV light callback is only called every second
-		//       if the UV light has changed since the last call!
-		uvl.set_uv_light_callback_period(1000);
+    // Set period for UV light receiver to 1s (1000ms).
+    // Note: The UV light callback is only called every second
+    //       if the UV light has changed since the last call!
+    uvl.set_uv_light_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  uv_light_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, uv_light_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,25 +10,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     let uvl = UvLightBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get threshold receivers with a debounce time of 10 seconds (10000ms).
-		uvl.set_debounce_period(10000);
+    // Get threshold receivers with a debounce time of 10 seconds (10000ms).
+    uvl.set_debounce_period(10000);
 
-     let uv_light_reached_receiver = uvl.get_uv_light_reached_callback_receiver();
+    let uv_light_reached_receiver = uvl.get_uv_light_reached_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `uvl` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for uv_light_reached in uv_light_reached_receiver {           
-                		println!("UV Light: {} mW/m²", uv_light_reached as f32 /10.0);
-		println!("UV Index > 3. Use sunscreen!");
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `uvl` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for uv_light_reached in uv_light_reached_receiver {
+            println!("UV Light: {} mW/m²", uv_light_reached as f32 / 10.0);
+            println!("UV Index > 3. Use sunscreen!");
+        }
+    });
 
-		// Configure threshold for UV light "greater than 75 mW/m²".
-		uvl.set_uv_light_callback_threshold('>', 75*10, 0);
+    // Configure threshold for UV light "greater than 75 mW/m²".
+    uvl.set_uv_light_callback_threshold('>', 75 * 10, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
